66211d5ed4c4e350b029573975a4f6b25b0f80ee
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-08-04 17:05:15 +1000

    added code in server.js file for middleware to parse incoming requests, codes for multiple html pages

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+const dbFilePath = path.join(__dirname, 'db/db.json');
+
+// HTML routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
