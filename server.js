8f3af84fcdf3905b7004febacc87fc365c348759
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-08-04 17:37:43 +1000

    added code for API routes and to handle errors

--- server.js
+++ server.js
@@ -59,3 +59,27 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error reading from the database.' });
+    }
+
+    const notes = JSON.parse(data);
+    const updateNotes = notes.filter((note) => note.id !== nodeId);
+
+    fs.writeFile(dbFilePath, JSON.stringify(updateNotes), (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error writing data to the database.' });
+      }
+
+      res.json({ message: 'Note deleted successfully.' });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server us running on ${PORT}`);
+});
